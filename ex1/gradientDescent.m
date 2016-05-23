3a67272242d8c7a5fdd7af053fee31d10db15801
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-05-23 17:01:57 +0200

    Add Linear One Variable Regression

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -15,14 +15,11 @@ for iter = 1:num_iters
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
-
-
-
+    
+    theta = theta - alpha * (1/m) * sum((X * theta - y) .* X)';
+    
+    
+    %theta(2) = theta - alpha * computeCost(X, y, theta) * X(:,2);
     % ============================================================
 
     % Save the cost J in every iteration    
